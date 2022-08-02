c3a34153b7dcfd9088e61400f9ec115444f351f6
Tensor <tensor@semantic-network.com>
2022-08-01 22:21:08 -0400

    change web3 proc to Vec<u8>

--- client/src/procedures/web3.rs
+++ client/src/procedures/web3.rs
@@ -29,7 +29,7 @@ impl<T> Procedure for Web3Procedures<T>
 where
     T: web3::Transport + Send + Sync,
 {
-    type Output = ProcedureOutput;
+    type Output = Vec<u8>;
 
     fn execute<R: Runner>(self, runner: &R) -> Result<Self::Output, ProcedureError> {
         use Web3Procedures::*;
