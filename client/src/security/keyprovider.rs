04e0bcb48d3b109dc376c56d7bb6f2361498bf18
Tensor <tensor@semantic-network.com>
2022-09-13 16:11:15 -0400

    update argon config to have parity with client argon.

--- client/src/security/keyprovider.rs
+++ client/src/security/keyprovider.rs
@@ -256,7 +256,14 @@ impl KeyProvider {
     where
         P: AsRef<[u8]> + Zeroize,
     {
-        let config = argon2::Config::default();
+        let config = argon2::Config {
+            lanes: 2,
+            mem_cost: 50_000,
+            time_cost: 30,
+            thread_mode: argon2::ThreadMode::from_threads(2),
+            variant: argon2::Variant::Argon2id,
+            ..Default::default()
+        };
 
         let key = argon2::hash_raw(passphrase.as_ref(), salt.as_ref(), &config)
             .map_err(|e| ClientError::Inner(e.to_string()))?;
